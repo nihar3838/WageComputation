6a0c2678e4d1caa5560ab1af58ea7757006eeaf5
nihar3838 <niharkhot123@gmail.com>
2024-06-13 17:30:07 +0530

    Part Time Employee
    Part time employee and his daily wage

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation Program");
-        int IS_FULL_TIME = 1;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation problem.");
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_FULL_TIME) {
+        int check = (int) ((Math.random() * 10) % 3);
+        if (check == IS_FULL_TIME){
             empHrs = 8;
-        } else {
+        }else if(check == IS_PART_TIME){
+            empHrs = 4;
+        }else{
             empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+        System.out.println("Employee wage: " + empWage);
     }
 }
